00109303ddaad66ff19b67367a58bc46d4347cd3
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-02-16 18:02:41 -0300

    Add Functions

--- functions.php
+++ functions.php
@@ -13,4 +13,37 @@
 
  echo "Some age: " . getAge(1997, 2022);
 
+ echo "<br>";
+
+ // Function wich prints "Hello I am Pedro"
+
+ function hello(string | null $name) : string
+ {
+     if (!isset($name)) {
+        return "Hello world <br>";
+     }
+     return "Hello $name <br>";
+ }
+
+ $name;
+ echo hello($name);
+ echo hello('Pedro');
+
+ // Sum function
+ function sum($a, $b)
+ {
+     return $a + $b;
+ }
+
+ echo "Sum " . sum(2, 3) . "<br>";
+
+ // Create function to sum all numbers using spread operator
+ function sumNumbers(...$numbers)
+ {
+    return array_reduce($numbers, fn($acc, $next) => $acc + $next, 0);
+ }
+
+ $sumResult = sumNumbers(1, 2, 3);
+ echo "Sum of numbers $sumResult";
+
 ?>
